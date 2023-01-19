72b18512bdbefc7b42f20b66ab8719a2ba30e63d
David Henao <daves.hb@hotmail.com>
2023-01-18 21:45:14 -0500

    Se modifica page home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
+import React from "react";
 
 export const Home = () => {
-
-
-
-    
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <div>Home</div>
+      <div>Home</div>
+    </>
+  );
+};
